557d94051225b564cd65ca8b7d6d3cbbd67a78c8
corlogix <corlogixco@gmail.com>
2021-09-20 14:22:23 -0400

    playing

---
+++ packages/veix/src/compiler.ts
@@ -0,0 +1,3 @@
+function veix(type, props = {}, ...children) {
+    return { type, props, children };
+}
---
+++ packages/veix/src/types/index.ts
@@ -0,0 +1,15 @@
+export type VeixNode = VeixElement | VeixText;
+export type VeixNodeList = VeixNode[];
+export type VeixElement = {
+  tag: string;
+  props: {
+    children: VeixNodeList;
+    etc;
+  };
+  className: string;
+  style: string;
+  events?: Object;
+  dom?: Node;
+}
+
+export type VeixText = string | number;
